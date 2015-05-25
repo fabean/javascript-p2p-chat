07dbd73debeba64c9afe62b7797dc18dc284685c
nphelpsvance <nphelps@vancepublishing.com>
2015-05-25 13:37:46 -0500

    We're trying to make friends.js

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -6,4 +6,17 @@ var peer = new Peer({ key: 'n0ei2j1souk57b9' });
 peer.on('open', function (id) {
   console.log('My peer ID is: ' + id);
 });
+
+var landline = peer.connect('dest-peer-id');
+
+peer.on('connection', function (landline) {
+  landline.on('open', function () {
+
+    landline.on('data', function (data) {
+      console.log(data);
+    });
+
+    landline.send('We\'re friends, right? Facebook BFF\'s!');
+  });
+});
 //# sourceMappingURL=scripts.js.map
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,5 +2,19 @@
 let peer = new Peer({key: 'n0ei2j1souk57b9'});
 
 peer.on('open', function(id) {
-  console.log('My peer ID is: ' + id);
+  console.log(`My peer ID is: ${id}`);
+});
+
+let landline = peer.connect('dest-peer-id');
+
+peer.on('connection', function(landline){
+  landline.on('open', function(){
+
+    landline.on('data', function(data){
+      console.log(data);
+    });
+
+    landline.send("We're friends, right? Facebook BFF's!");
+
+  });
 });
