5caa89d530c939525bc229050819a4246ed3f307
Josh Fabean <josh@codekoalas.com>
2015-05-25 13:39:15 -0500

    added some html

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,10 +1,16 @@
+/* jshint ignore:start */
 /* jshint strict: true */
 
 'use strict';
 
-var peer = new Peer({ key: 'n0ei2j1souk57b9' });
+var peer = new Peer({ key: 'n0ei2j1souk57b9' }),
+    joinHostButton = document.getElementById('join-host');
 
 peer.on('open', function (id) {
   console.log('My peer ID is: ' + id);
+});
+
+joinHostButton.addEventListener('click', function () {
+  peer.connect(document.getElementById('friends-peer-id').value);
 });
 //# sourceMappingURL=scripts.js.map
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,7 +1,13 @@
+/* jshint ignore:start */
 /* jshint strict: true */
 
-let peer = new Peer({key: 'n0ei2j1souk57b9'});
+let peer = new Peer({key: 'n0ei2j1souk57b9'}),
+    joinHostButton = document.getElementById('join-host');
 
 peer.on('open', function(id) {
   console.log(`My peer ID is: ${id}`);
+});
+
+joinHostButton.addEventListener('click', function(){
+  peer.connect(document.getElementById('friends-peer-id').value);
 });
