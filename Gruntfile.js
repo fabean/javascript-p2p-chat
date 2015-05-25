9dd06854d21b54b3482fea688be9d7108a94ec67
Josh Fabean <josh@codekoalas.com>
2015-05-25 12:05:49 -0500

    basic file structure, grunt does not work yet

---
+++ Gruntfile.js
@@ -0,0 +1,109 @@
+'use strict';
+module.exports = function (grunt) {
+    require('time-grunt')(grunt);
+    require('load-grunt-tasks')(grunt);
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        src: 'src',
+        dist: 'dist',
+        sass: {
+            'dist': {
+                'options': {
+                    'style': 'compressed',
+                    'quiet': false,
+                    'compass': false,
+                },
+                'files': { '<%= dist %>/css/styles.css': '<%= src %>/scss/styles.scss' }
+            }
+        },
+        autoprefixer: {
+            'options': {
+                'browsers': [
+                    'last 2 versions',
+                ],
+                'remove': false
+            },
+            'dist': { 'src': '<%= dist %>/css/styles.css' }
+        },
+        jade: {
+            'compile': {
+                'options': {
+                    'pretty': true,
+                    'data': { 'debug': false }
+                },
+                'files': [{
+                        'expand': true,
+                        'cwd': '<%= src %>/',
+                        'src': [
+                            '*.jade',
+                            '!_*.jade'
+                        ],
+                        'ext': '.html',
+                        'dest': '<%= dist %>/'
+                    }]
+            }
+        },
+        jshint: {
+            'options': { 'jshintrc': '.jshintrc' },
+            'all': [
+                'Gruntfile.js',
+                '<%= src %>/js/**/*.js'
+            ]
+        },
+        uglify: {
+            'options': {
+                'preserveComments': 'some',
+                'mangle': false
+            },
+            'dist': { 'files': { '<%= dist %>/js/scripts.js': ['<%= src %>/js/scripts.js'] } }
+        },
+        watch: {
+            'grunt': {
+                'files': ['Gruntfile.js'],
+                'tasks': [
+                    'sass',
+                    'jshint'
+                ]
+            },
+            'sass': {
+                'files': '<%= src %>/scss/**/*.scss',
+                'tasks': [
+                    'sass',
+                    'autoprefixer'
+                ]
+            },
+            'jade': {
+                'files': '<%= src %>/**/*.jade',
+                'tasks': ['jade']
+            },
+            'livereload': {
+                'files': [
+                    '<%= src %>/**/*.jade',
+                    '<%= src %>/js/**/*.js',
+                    '<%= src %>/scss/**/*.scss',
+                ],
+                'options': { 'livereload': true }
+            }
+        },
+        connect: {
+            'dist': {
+                'options': {
+                    'port': 9001,
+                    'base': '<%= dist %>/',
+                    'open': true,
+                    'keepalive': false,
+                    'livereload': true,
+                    'hostname': '127.0.0.1'
+                }
+            }
+        }
+    });
+    grunt.registerTask('default', [
+        'jade',
+        'sass',
+        'autoprefixer',
+        'jshint',
+        'connect',
+        'watch'
+    ]);
+};
